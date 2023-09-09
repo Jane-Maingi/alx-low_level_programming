01cfc38de70048910ca2049998ad8f16c12236b7
Jane-Maingi <dianamaingi0@gmail.com>
2023-09-09 04:15:00 -0700

    --amend

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-printf("Size of int: %lu bytes\n", sizeof(int));
-printf("Size of char: %lu bytes\n", sizeof(char));
-printf("Size of float: %lu bytes\n", sizeof(float));
-printf("Size of long int: %lu bytes\n", sizeof(long int));
-printf("Size of long long int: %lu bytes\n", sizeof(long long int));
+printf("Size of an int: %lu bytes\n", sizeof(int));
+printf("Size of a char: %lu bytes\n", sizeof(char));
+printf("Size of a float: %lu bytes\n", sizeof(float));
+printf("Size of a long int: %lu bytes\n", sizeof(long int));
+printf("Size of a long long int: %lu bytes\n", sizeof(long long int));
 return (0);
 }
