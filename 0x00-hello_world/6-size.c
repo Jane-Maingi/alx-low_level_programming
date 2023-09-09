2ec98bc9e9d8e13f0416fd9cb901a09b48d5b507
Jane-Maingi <dianamaingi0@gmail.com>
2023-09-09 03:36:28 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return:0 always
+ */
+
+int main(void)
+{
+printf("Size of int: %lu bytes\n", sizeof(int));
+printf("Size of char: %lu bytes\n", sizeof(char));
+printf("Size of float: %lu bytes\n", sizeof(float));
+printf("Size of double: %lu bytes\n", sizeof(double));
+return (0);
+}
